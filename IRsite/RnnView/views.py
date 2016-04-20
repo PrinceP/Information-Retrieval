8d32cfbe56a4b6a2111953a959d587f8cd32f858
PrinceP <prince.patel.14@outlook.com>
2016-04-20 14:03:50 +0530

    Commit

--- IRsite/RnnView/views.py
+++ IRsite/RnnView/views.py
@@ -7,8 +7,13 @@ from django.http import Http404, HttpResponse, HttpResponseRedirect
 from rnnmodel import returnModel,returnDictionary
 import numpy
 
+city_name = ['agartala', 'agra', 'ahmedabad', 'allahabad', 'amritsar', 'aurangabad', 'bagdogra', 'bangalore', 'bhavnagar', 'bhopal', 'bhubaneswar', 'bhuj', 'calcutta', 'chandigarh', 'chennai', 'cochin', 'coimbatore', 'daman', 'dehradun', 'dibrugarh', 'dimapur', 'diu', 'gauhati', 'goa', 'gwalior', 'hubli', 'hyderabad', 'imphal', 'indore', 'jaipur', 'jammu', 'jamnagar', 'jamshedpur', 'jodhpur', 'jorhat', 'kanpur', 'khajuraho', 'kozhikode', 'leh', 'lucknow', 'ludhiana', 'madurai', 'mangalore', 'mumbai', 'nagpur', 'nanded', 'nasik', 'patna', 'pondicherry', 'pune', 'porbandar', 'puttaparthi', 'rajkot', 'ranchi', 'shillong', 'silchar', 'srinagar', 'surat', 'tezpur', 'tiruchirapally', 'tirupati', 'trivandrum', 'udaipur', 'vadodara', 'varanasi', 'vijayawada', 'vishakhapatnam', 'new delhi', 'port blair', 'rae bareli']
+rnn = returnModel()
+dicts = returnDictionary()
+
+
+
 def contextwin(l, win):
-	
 	assert (win % 2) == 1
 	assert win >= 1
 	l = list(l)
@@ -21,8 +26,8 @@ def contextwin(l, win):
 
 def searchterm(request):
 	#Load the model and dictionary
-	rnn = returnModel()
-	dicts = returnDictionary()
+	
+
 
 	if request.POST:
 		array = []
@@ -31,19 +36,26 @@ def searchterm(request):
 		finalquery = finalquery.split()
 		array = []
 		for z in finalquery:
-			array.append(dicts['words2idx'][z])
-		print array
+			try:
+				array.append(dicts['words2idx'][z.lower()])
+			except KeyError:
+				if z.lower() in city_name:
+					array.append(88)
+
+
+		
+
 		query = numpy.asarray(array).astype('int32')
 		prediction = rnn.classify(contextwin(query,7))
-		print prediction
+		#print prediction
 		array = []
 		for x in prediction:
 			key = dicts['labels2idx'].keys()[dicts['labels2idx'].values().index(x)]
 			array.append(key)
 		finalquery = '   '.join(array)
 		return render_to_response('search.html', {'result': (finalquery)  } )
-	
 	else:
 		return render_to_response('search.html' ,{'result': None })
+
 	
 
